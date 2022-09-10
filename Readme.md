3d24e42ed02ad50bfe1f6888fab7baeee49b1004
Sidharth Das <sdas.engineer@gmail.com>
2022-09-10 15:30:14 +0100

    Initialized and deployed repo

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Cricket Scoreboard - Javascript
+
+[![GitHub license](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
+
+This simple web application is designed to log and display cricket scores. This application can be used to display scores in local tournaments and matches played between hobbyists.
+
+## Demo
+
+You can view a demo of the application [here](https://pip.pypa.io/en/stable/).
+
+## How to modify
+
+To modify team names, simply head over to the index.html file and make the following changes in line 16 & 17.
+
+```html
+<div id = "teams">
+<h1>TEAM 1</h2>  //Change this line
+<h1>TEAM 2</h2> //Change this line
+</div>
+```
+
+## Screenshot
+
+## Contributing
+
+If you wish to submit improvements to the javascript, or the CSS, then I welcome forks & pull requests.
+
+## License
+
+Cricket Scoreboard is released under [MIT](https://choosealicense.com/licenses/mit/) license.
---
+++ index.js
@@ -0,0 +1,55 @@
+let teamA = 0
+let teamB = 0
+
+let scoreA = document.getElementById("score-1")
+let scoreB = document.getElementById("score-2")
+
+function addScoreA1(){
+    teamA = teamA + 1
+    scoreA.textContent = teamA
+}
+
+function addScoreA2(){
+    teamA = teamA + 2
+    scoreA.textContent = teamA
+}
+
+function addScoreA3(){
+    teamA = teamA + 3
+    scoreA.textContent = teamA
+}
+
+function addScoreA4(){
+    teamA = teamA + 4
+    scoreA.textContent = teamA
+}
+
+function addScoreA6(){
+    teamA = teamA + 6
+    scoreA.textContent = teamA
+}
+
+function addScoreB1(){
+    teamB = teamB + 1
+    scoreB.textContent = teamB
+}
+
+function addScoreB2(){
+    teamB = teamB + 2
+    scoreB.textContent = teamB
+}
+
+function addScoreB3(){
+    teamB = teamB + 3
+    scoreB.textContent = teamB
+}
+
+function addScoreB4(){
+    teamB = teamB + 4
+    scoreB.textContent = teamB
+}
+
+function addScoreB6(){
+    teamB = teamB + 6
+    scoreB.textContent = teamB
+}
